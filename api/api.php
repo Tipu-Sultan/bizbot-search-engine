9b749da877248b83443f81d7849e0ac927c9171a
Tipu Sultan <tipu@student.iul.ac.in>
2024-01-02 19:18:33 +0530

    add and fix

--- api/api.php
+++ api/api.php
@@ -4,7 +4,7 @@ include 'db.php';
 if (isset($_GET['query'])) {
     $query = $_GET['query'];
     $sql = "SELECT * FROM products WHERE title LIKE '%$query%' OR description LIKE '%$query%'";
-    $result = $conn->query($sql);
+    $result = mysqli_query($conn,$sql);
 
     $html = '';
 
@@ -12,10 +12,10 @@ if (isset($_GET['query'])) {
         while ($product = $result->fetch_assoc()) {
             $html .= '<a href="product_details.php?id=' . $product['id'] . '" class="text-decoration-none">';
             $html .= '<div class="row pt-2 w-50 m-2">';
-            $html .= '<div class="col-lg-4 col-md-6 col-sm-12">';
+            $html .= '<div class="col-lg-4 col-md-4 col-sm-4">';
             $html .= '<img src="images/' . $product['image_link'] . '" alt="' . $product['title'] . '" class="img-fluid rounded" width="150" height="150">';
             $html .= '</div>';
-            $html .= '<div class="col-lg-8 col-md-6 col-sm-12">';
+            $html .= '<div class="col-lg-8 col-md-8 col-sm-6 ">';
             $html .= '<div class="d-flex flex-column h-100">';
             $html .= '<h3 class="text-dark mb-3">' . $product['title'] . '</h3>';
             $html .= '<p class="text-dark">' . $product['description'] . '</p>';
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Search</title>
-    <!-- Include Bootstrap CSS -->
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
@@ -18,7 +18,7 @@
     <div class="container text-center">
         <h1 class="mb-4">Product Search</h1>
         <div class="row justify-content-center">
-            <div class="col-12 col-md-8 mb-3">
+            <div class="col-lg-8 col-md-8 col-sm-12 col-12 mb-3">
                 <div class="input-group">
                     <input type="text" id="searchInput" class="form-control rounded-pill"
                         placeholder="Search for products..." oninput="searchProducts()">
